eef07a887e189b0dd448772e0abb3e03426981cd
brian <rbrianhazzard@gmail.com>
2026-06-06 18:35:41 -0400

    feat: enable steel and thunder units. this means steel and thunder is no longer required as a separate mod

--- sql/BBG_Expanded/MOARUnits_enabled.sql
+++ sql/BBG_Expanded/MOARUnits_enabled.sql
@@ -1,5 +1,3 @@
-UPDATE EnabledUniqueUnits SET Enabled = 0;
-
 UPDATE EnabledUniqueUnits SET Enabled = 1 WHERE Type IN ('UNIT_GERMAN_PANZER', 'UNIT_ROMAN_ONAGER');
 UPDATE EnabledUniqueUnits SET OwnerType='LEADER_BARBAROSSA' WHERE Type='UNIT_GERMAN_PANZER';
 UPDATE EnabledUniqueUnits SET OwnerType='LEADER_JULIUS_CAESAR' WHERE Type='UNIT_ROMAN_ONAGER';
